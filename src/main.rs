ce8f26c0f26ca80a2411e718074a255fca3789c8
SM1701 <717513736@qq.com>
2024-05-15 11:18:05 +0800

    ch1.4:多国语言

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    greet_world()
+}
+
+fn greet_world() {
+    let chinese = "世界，你好";
+    let english = "World, hello";
+    let regions = [chinese, english];
+    for region in regions {
+        println!("{}", region);
+    }
 }
