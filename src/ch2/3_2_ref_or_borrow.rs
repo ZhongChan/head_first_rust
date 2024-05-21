ecaddae9f303b9c1fbd7fd74b3da36f70ef7234f
SM1701 <717513736@qq.com>
2024-05-21 21:43:41 +0800

    2.3所有权和借用：nll

--- src/ch2/3_2_ref_or_borrow.rs
+++ src/ch2/3_2_ref_or_borrow.rs
@@ -4,6 +4,7 @@ fn main() {
         ("不可变引用", Box::new(|| not_mut_ref())),
         ("可变引用", Box::new(|| mut_ref())),
         ("可变引用同时只有有一个", Box::new(|| only_one_mut_ref())),
+        ("NLL", Box::new(|| nll())),
     ];
 
     for (name, function) in functions.into_iter() {
@@ -57,4 +58,18 @@ fn only_one_mut_ref() {
     }
     let t3 = &mut t1;
     println!("t3:{}", t3);
+}
+
+// None-Lexical Lifetimes
+fn nll() {
+    let mut s1 = String::from("hello");
+    let s2 = &s1;
+    let s3 = &s1;
+    //s2 s3 是多个不可变引用，且生命周期结束(最后一次使用)
+    //println 只是对 s2 s3的借用
+    println!("s2:{},s3:{}", s2, s3);
+
+
+    let s4 = &mut s1; //只能由一个可变引用
+    println!("s4:{}", s4);
 }
