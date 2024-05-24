a2f80d16deb914f9ebc7d55a0f1dc1746107f87e
SM1701 <717513736@qq.com>
2024-05-24 18:08:03 +0800

    2.4 复合类型:字符串转义和保持原文

--- src/ch2/4_1_string_slice.rs
+++ src/ch2/4_1_string_slice.rs
@@ -38,7 +38,9 @@ fn main() {
         ("String与&str的转换", Box::new(|| string_str())),
         ("字符串索引", Box::new(|| string_index())),
         ("字符串切片", Box::new(|| string_slice())),
-        ("字符操作", Box::new(|| string_operating())),
+        ("字符串操作", Box::new(|| string_operating())),
+        ("字符串转义", Box::new(|| string_escape())),
+        ("字符串不转义", Box::new(|| string_unescape())),
     ];
 
     for (name, function) in functions.into_iter() {
@@ -222,4 +224,39 @@ fn string_operating() {
     let s11 = String::from("rust");
     let s12 = format!("{}{}!", s10, s11);
     println!("{}", s12);
+}
+
+fn string_escape() {
+    // 通过 \ + 字符的十六进制表示，转义输出一个字符
+    let byte_escape = "I'm writing \x52\x75\x73\x74!";
+    println!("What are you doing\x3F (\\x3F means ?) {}", byte_escape);
+
+    // \u 可以输出一个 unicode 字符
+    let unicode_codepoint = "\u{211D}";
+    let character_name = "\"DOUBLE-STRUCK CAPITAL R\"";
+    println!(
+        "Unicode character {} (U+211D) is called {}",
+        unicode_codepoint, character_name
+    );
+
+    //使用 \ 忽略换行
+    let long_string = "String literals
+                        can span multiple lines.
+                        The linebreak and indentation here ->\
+                        <- can be escaped too!";
+    println!("{}", long_string);
+}
+
+fn string_unescape() {
+    println!("{}", "hello \\x52\\x75\\x73\\x74");
+    let raw_str = r"Escapes don't work here: \x3F \u{211D}";
+    println!("{}", raw_str);
+
+    // 如果字符串包含双引号，可以在开头和结尾加 #
+    let quotes = r#"And then I said: "There is no escape!""#;
+    println!("{}", quotes);
+
+    // 如果还是有歧义，可以继续增加，没有限制
+    let longer_delimiter = r###"A string with "# in it. And even "##!"###;
+    println!("{}", longer_delimiter);
 }
