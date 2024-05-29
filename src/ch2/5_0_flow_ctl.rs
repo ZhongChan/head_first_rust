8bbd59df1611687a6fa84e0cbeb3a5162524617f
SM1701 <717513736@qq.com>
2024-05-29 13:49:15 +0800

    2.5 流程控制:while loop

--- src/ch2/5_0_flow_ctl.rs
+++ src/ch2/5_0_flow_ctl.rs
@@ -3,6 +3,8 @@ fn main() {
         ("if 分支控制", Box::new(|| if_ctl())),
         ("else if 分支控制", Box::new(|| else_if_ctl())),
         ("for 循环", Box::new(|| for_loop())),
+        ("while 循环", Box::new(|| while_loop())),
+        ("loop 循环", Box::new(|| the_loop())),
     ];
 
     for (name, function) in functions.into_iter() {
@@ -55,4 +57,72 @@ fn for_loop() {
         println!("{}", x);
     }
     println!("{:?}", str); // &str 实现了 Copy
+
+    let a = [1, 2, 3, 4, 5];
+    // k v 迭代器
+    for (i, v) in a.iter().enumerate() {
+        println!("第 {} 个元素是 {}", i + 1, v);
+    }
+
+    // continue
+    for x in 1..4 {
+        if x == 2 {
+            continue;
+        }
+        println!("{}", x);
+    }
+
+    for x in 1..5 {
+        if x == 2 {
+            break;
+        }
+        println!("{}", x);
+    }
+}
+
+fn while_loop() {
+    let mut n = 0;
+    while n < 5 {
+        println!("{}!", n);
+        n = n + 1;
+    }
+    println!("我从 while 出来了啦！");
+
+    let mut m = 0;
+    loop {
+        if m > 5 {
+            break;
+        }
+        println!("{}!!", m);
+        m += 1;
+    }
+    println!("我从 loop 出来了！");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+    while index < a.len() {
+        println!("the value is : {}", a[index]);
+        index = index + 1;
+    }
+
+    println!("while ending!");
+
+    for element in a.iter() {
+        println!("the value is : {}", element);
+    }
+}
+
+
+/// loop 不要轻易使用
+/// break 可以单独使用，也可以带一个返回值，有些类似 return
+/// loop 是一个表达式，因此可以返回一个值
+fn the_loop() {
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    dbg!(result);
 }
