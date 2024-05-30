50d2cbd5d8532891d147c6c8092c9d62521b57fa
SM1701 <717513736@qq.com>
2024-05-30 14:49:31 +0800

    2.6 模式匹配：模式使用场景

---
+++ src/ch2/6_3_match_scene.rs
@@ -0,0 +1,87 @@
+fn main() {
+    let functions: Vec<(&str, Box<dyn Fn()>)> = vec![
+        ("基本示例", Box::new(|| basic())),
+        ("while let", Box::new(|| while_let())),
+        ("for 循环", Box::new(|| for_loop())),
+        ("let 语句", Box::new(|| let_expression())),
+    ];
+
+    for (name, function) in functions.into_iter() {
+        println!();
+        println!(">>>>>>>>>>开始执行：{}", name);
+        function();
+        println!("{}: 执行结束<<<<<<<<<<", name);
+    }
+}
+
+/// 模式
+/// 模式是 Rust 中的特殊语法，它用来匹配类型中的结构和数据，它往往和 match 表达式联用，以实现强大的模式匹配能力。模式一般由以下内容组合而成：
+///
+/// * 字面值
+/// * 解构的数组、枚举、结构体或者元组
+/// * 变量
+/// * 通配符
+/// * 占位符
+/// # 所有可能用到模式的地方
+///
+/// ## match 分支
+///```
+/// match VALUE {
+///     PATTERN => EXPRESSION,
+///     PATTERN => EXPRESSION,
+///     PATTERN => EXPRESSION,
+/// }
+///```
+/// 如上所示，match 的每个分支就是一个模式，因为 match 匹配是穷尽式的，因此我们往往需要一个特殊的模式 _，来匹配剩余的所有情况：
+/// ```
+/// match VALUE {
+///     PATTERN => EXPRESSION,
+///     PATTERN => EXPRESSION,
+///     _ => EXPRESSION,
+/// }
+/// ```
+/// ## if let 分支
+/// if let 往往用于匹配一个模式，而忽略剩下的所有模式的场景：
+/// ```
+/// if let PATTERN = SOME_VALUE {
+///
+/// }
+/// ```
+///
+///
+///
+///
+fn basic() {}
+
+
+fn while_let() {
+    let mut stack = Vec::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    // stack.pop() 返回 Option<T>
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+}
+
+
+/// # for 循环
+/// 这里使用 enumerate 方法产生一个迭代器，该迭代器每次迭代会返回一个 (索引，值) 形式的元组，然后用 (index,value) 来匹配。
+fn for_loop() {
+    let v = vec!['a', 'b', 'c'];
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+}
+
+/// # let 语句
+/// ```let PATTERN = EXPRESSION;```
+fn let_expression() {
+    let (x, y, z) = (1, 2.0, 'c');
+    dbg!((x, y, z));
+
+    // let (r,g,b) = (1,2); //Type mismatch [E0308]
+    // let (r, g) = (1, 2, 3); //Type mismatch [E0308]
+}
