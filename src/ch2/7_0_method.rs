17da6e9cd4d5880681ce560d653cfe7cff0b52a7
SM1701 <717513736@qq.com>
2024-05-30 21:36:29 +0800

    2.7 方法:所有权

--- src/ch2/7_0_method.rs
+++ src/ch2/7_0_method.rs
@@ -1,6 +1,7 @@
 fn main() {
     let functions: Vec<(&str, Box<dyn Fn()>)> = vec![
         ("基本示例", Box::new(|| basic())),
+        ("所有权", Box::new(|| owner_ship())),
     ];
 
     for (name, function) in functions.into_iter() {
@@ -17,14 +18,17 @@ fn basic() {
 
     let r = Rectangle::new(4f64, 9f64);
     println!("{}", r.area());
+    println!("{}", r.perimeter());
 }
 
+#[allow(dead_code)]
 struct Circle {
     x: f64,
     y: f64,
     radius: f64,
 }
 
+#[allow(dead_code)]
 impl Circle {
     pub fn new(x: f64, y: f64, radius: f64) -> Self {
         Self { x, y, radius }
@@ -51,5 +55,52 @@ impl Rectangle {
 
     fn perimeter(self: &Self) -> f64 {
         (self.width + self.height) * 2f64
+    }
+}
+
+/// self 依然有所有权的概念：
+/// * ```self``` 表示所有权转移到该方法中，这种形式用的较少
+/// * ```&self``` 表示该方法对实例的不可变借用
+/// * ```&mut self``` 表示可变借用
+fn owner_ship() {
+    let inst1 = Example::new("hello".to_string());
+    inst1.consume();
+    // println!("{}", inst1.data); //error[E0382]: borrow of moved value: `inst1`
+
+    let inst2 = Example::new("Hi".to_string());
+    inst2.read();
+    println!("Still here: {}", inst2.data);
+
+    let mut inst3 = Example::new("Hello".to_string());
+    inst3.modified();
+    println!("{}", inst3.data);
+}
+
+struct Example {
+    data: String,
+}
+
+#[allow(dead_code)]
+impl Example {
+    pub fn new(data: String) -> Self {
+        Self { data }
+    }
+    pub fn data(&self) -> &str {
+        &self.data
+    }
+    pub fn set_data(&mut self, data: String) {
+        self.data = data;
+    }
+
+    fn consume(self) {
+        println!("Consuming data {}", self.data);
+    }
+
+    fn read(&self) {
+        println!("Reading data {}", self.data);
+    }
+
+    fn modified(&mut self) {
+        self.data.push_str(",Rust")
     }
 }
