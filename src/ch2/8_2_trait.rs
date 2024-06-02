9aeb7a45ed178d23d94d1455e9c6cba8b9fbd11f
SM1701 <717513736@qq.com>
2024-06-02 17:00:21 +0800

    2.8 trait： 孤儿规则

--- src/ch2/8_2_trait.rs
+++ src/ch2/8_2_trait.rs
@@ -1,6 +1,9 @@
+use std::fmt::{Display, Formatter};
+
 fn main() {
     let functions: Vec<(&str, Box<dyn Fn()>)> = vec![
         ("基本示例", Box::new(|| basic())),
+        ("孤儿规则", Box::new(|| orphan_rule())),
     ];
 
     for (name, function) in functions.into_iter() {
@@ -16,6 +19,13 @@ fn basic() {
     println!("{}", post.summarize());
     let weibo = Weibo::new("重".to_string(), "我发了一条微博".to_string());
     println!("{}", weibo.summarize());
+}
+
+/// # 孤儿规则（Orphan Rule）
+/// * 如果你想要为类型 A 实现特征 T，那么 A 或者 T 至少有一个是在当前作用域中定义的！
+fn orphan_rule() {
+    let post = Post::new("Head first Rust".to_string(), "Zhong".to_string(), "一本介绍Rust的书籍".to_string());
+    println!("{}", post);
 }
 
 pub trait Summary {
@@ -38,6 +48,17 @@ impl Post {
 impl Summary for Post {
     fn summarize(&self) -> String {
         format!("文章：{}，作者：{}", self.title, self.author)
+    }
+}
+
+/// 为 Post 实现 Display 特征
+impl Display for Post {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Title: {}\nAuthor: {}\nContent: {}",
+            self.title, self.author, self.content,
+        )
     }
 }
 
