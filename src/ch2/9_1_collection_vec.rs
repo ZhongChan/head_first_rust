23d1417ea25ede116485d7c2bb2fd195cf7a81f9
SM1701 <717513736@qq.com>
2024-06-05 21:53:49 +0800

    2.9 集合类型: Vector

---
+++ src/ch2/9_1_collection_vec.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let functions: Vec<(&str, Box<dyn Fn()>)> = vec![("关联类型", Box::new(|| basic()))];
+
+    for (name, function) in functions.into_iter() {
+        println!();
+        println!(">>>>>>>>>>开始执行：{}", name);
+        function();
+        println!("{}: 执行结束<<<<<<<<<<", name);
+    }
+}
+
+fn basic() {}
