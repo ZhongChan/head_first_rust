705b1d0ec8f52e01b33fb942bc47fcadff3f5983
SM1701 <717513736@qq.com>
2024-06-11 17:59:07 +0800

    2.9 集合类型: Vector 常用方法

--- src/ch2/9_1_collection_vec.rs
+++ src/ch2/9_1_collection_vec.rs
@@ -2,6 +2,7 @@ fn main() {
     let functions: Vec<(&str, Box<dyn Fn()>)> = vec![
         ("关联类型", Box::new(|| basic())),
         ("存储不同类型元素", Box::new(|| diff_element())),
+        ("常用方法", Box::new(|| common_usage())),
     ];
 
     for (name, function) in functions.into_iter() {
@@ -100,4 +101,60 @@ impl IpAddr2 for V6 {
     fn display(&self) {
         println!("ipv6: {:?}", self.0)
     }
+}
+
+/// # Vector 常用方法
+fn common_usage() {
+    //初始化
+    let v1 = vec![0; 3];
+    let v2 = Vec::from([0, 0, 0]);
+    assert_eq!(v1, v2);
+
+    //扩容 减少内存拷贝
+    let mut v3 = Vec::with_capacity(10);
+    v3.extend([1, 2, 3]);
+    println!("v3的长度是：{}，容量是：{}", v3.len(), v3.capacity());
+
+    v3.reserve(100); //预留空间
+    println!("v3 扩容后的长度是：{}，容量是：{}", v3.len(), v3.capacity());
+
+    v3.shrink_to_fit();
+    println!("v3 是否容量后的长度是：{}，容量是：{}", v3.len(), v3.capacity());
+
+    //常用方法
+    let mut v4 = vec![1, 2];
+    assert!(!v4.is_empty());
+
+    v4.insert(2, 5); // 在指定索引插入数据，索引值不能大于 v4 的长度， v: [1, 2, 5]
+    assert_eq!(v4.remove(1), 2); //删除指定索引元素，返回值
+    assert_eq!(v4.pop(), Some(5));  //弹出尾部元素
+    assert_eq!(v4.pop(), Some(1)); //数组为空
+    assert!(v4.is_empty());
+    assert_eq!(v4.pop(), None);
+    v4.clear();
+
+    let mut v5 = [11, 3, 22].to_vec();
+    v4.append(&mut v5); //v5所有元素添加到v4
+    assert_eq!(v4, [11, 3, 22]);
+    assert_eq!(v5, []);
+    v4.truncate(2); //截取到指定长度
+    assert_eq!(v4, [11, 3]);
+    v4.retain(|x| *x > 10); //保留满足条件的元素
+    assert_eq!(v4, [11]);
+
+    let mut v6 = vec![11, 22, 33, 44, 55];
+    // 删除指定范围的元素，同事获取被删除元素的迭代器：v6:[11,55] m:[22,33,44]
+    let mut m: Vec<_> = v6.drain(1..=3).collect();
+    assert_eq!(v6, [11, 55]);
+    assert_eq!(m, [22, 33, 44]);
+
+    //指定索引位置分割数组
+    let v7 = m.split_off(1);
+    assert_eq!(m, [22]);
+    assert_eq!(v7, [33, 44]);
+
+    //数组切片
+    let v8 = vec![11, 22, 33, 44, 55];
+    let v8_slice = &v8[1..=3]; //从 index 取 size 个元素
+    assert_eq!(v8_slice, [22, 33, 44]);
 }
