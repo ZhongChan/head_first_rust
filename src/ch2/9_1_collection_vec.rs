84fe8218e2b9ec70f5435fd5c0f761e3c18d01c5
SM1701 <717513736@qq.com>
2024-06-11 18:22:07 +0800

    2.9 集合类型: Vector 结构体排序

--- src/ch2/9_1_collection_vec.rs
+++ src/ch2/9_1_collection_vec.rs
@@ -162,16 +162,32 @@ fn common_usage() {
 
 /// # Vector 排序
 fn vec_sort() {
+    i32_sort();
+    f32_no_nan();
+    f32_nan();
+    unstable_stable();
+    struct_sort();
+    struct_default_sort();
+}
+
+/// # 整数数组排序
+fn i32_sort() {
     let mut i32_v = vec![1, 7, 9, 2, 11];
     i32_v.sort_unstable(); //非稳定排序，比 稳定排序速度快 空间少
     assert_eq!(i32_v, [1, 2, 7, 9, 11]);
+}
 
-
+/// # 浮点数 无 NAN 排序
+fn f32_no_nan() {
     let mut f32_v = vec![1.0, 5.6, 10.3, 2.0, 15f32];
     // f32_v.sort_unstable(); //Trait `Ord` is not implemented for `f32` [E0277] 浮点数有 NAN
     f32_v.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap()); //我们确定在我们的浮点数数组当中，不包含 NAN 值，那么我们可以使用 partial_cmp 来作为大小判断的依据。
     assert_eq!(f32_v, vec![1.0, 2.0, 5.6, 10.3, 15f32]);
+}
 
+/// # 浮点数 有 NAN 排序
+/// 自定义排序
+fn f32_nan() {
     let mut nan_v = vec![1.0, 5.6, 10.3, 2.0, 15.0, f32::NAN, 3.3, f32::NAN];
     nan_v.sort_unstable_by(|a, b| {
         match (a.is_nan(), b.is_nan()) {
@@ -182,8 +198,10 @@ fn vec_sort() {
         }
     });
     println!("{:?}", nan_v); //NAN 通常在开头或末尾
+}
 
-
+/// # 稳定排序和非稳定排序
+fn unstable_stable() {
     let items = vec![
         Item { value: 5, original_index: 1 },
         Item { value: 3, original_index: 2 },
@@ -203,8 +221,58 @@ fn vec_sort() {
     println!("After sort_stable: {:?}", items_stable);
 }
 
+/// # 结构体排序
+fn struct_sort() {
+    let mut people = vec![
+        Person::new("Foo".to_string(), 32),
+        Person::new("FooBar".to_string(), 19),
+        Person::new("AFoo".to_string(), 45),
+    ];
+    people.sort_unstable_by(|a, b| a.age.cmp(&b.age));
+    println!("{:?}", people)
+}
+
+/// 结构体使用默认 Trait 排序
+fn struct_default_sort() {
+    let mut animals = vec![
+        Animal::new("Dog".to_string(), 1),
+        Animal::new("Snake".to_string(), 3),
+        Animal::new("Cat".to_string(), 2),
+    ];
+    // 根据结构体定义字段的顺序，选择排序字段
+    // 目前是 age
+    animals.sort_unstable();
+    println!("{:?}", animals);
+}
+
 #[derive(Debug, PartialEq, Eq, Clone)]
+#[allow(dead_code)]
 struct Item {
     value: i32,
     original_index: usize,
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Person {
+    name: String,
+    age: u32,
+}
+
+impl Person {
+    pub fn new(name: String, age: u32) -> Self {
+        Self { name, age }
+    }
+}
+
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
+struct Animal {
+    age: i32,
+    category: String,
+}
+
+impl Animal {
+    pub fn new(category: String, age: i32) -> Self {
+        Self { category, age }
+    }
 }
