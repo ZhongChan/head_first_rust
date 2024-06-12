dbbbb819cfb864db950900172acc8f75aa757e41
SM1701 <717513736@qq.com>
2024-06-12 09:10:02 +0800

    2.9 集合类型: HashMap 创建

--- src/ch2/9_2_collection_hashmap.rs
+++ src/ch2/9_2_collection_hashmap.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let functions: Vec<(&str, Box<dyn Fn()>)> = vec![
         ("基本示例", Box::new(|| basic())),
@@ -11,4 +13,23 @@ fn main() {
     }
 }
 
-fn basic() {}
+fn basic() {
+    // 基本创建
+    let mut map = HashMap::new();
+    map.insert("ruby", 1);
+    map.insert("Emerald", 2);
+    map.insert("Topaz", 5);
+    println!("{:?}", map);
+
+
+    // 数组转化
+    let team_list = vec![
+        ("中国队", 100),
+        ("美国队", 99),
+        ("日本队", 1),
+    ];
+
+    // 需要手动标注类型为：HashMap<_,_>
+    let team_map: HashMap<_, _> = team_list.into_iter().collect();
+    println!("{:?}", team_map)
+}
