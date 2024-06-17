fe342c75e0d3fc5dfcda1ddb1f27c68a644b7339
SM1701 <717513736@qq.com>
2024-06-17 22:15:16 +0800

    2.11 返回值和错误处理: result

---
+++ src/ch2/11_2_result.rs
@@ -0,0 +1,16 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn main() {
+    let result = File::open("foo.txt");
+    let f = result.unwrap_or_else(|err| match err.kind() {
+        ErrorKind::NotFound => File::create("foo.txt").unwrap_or_else(|err| {
+            panic!("Problem creating file: {:?}", err)
+        }),
+
+        other_err => {
+            panic!("Problem opening file: {:?}", other_err)
+        }
+    });
+    println!("Is File: {}", f.metadata().unwrap().is_file())
+}
