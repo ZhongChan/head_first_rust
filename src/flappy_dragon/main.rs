3a1163295a381dd6a6561cb22dd0711b8435031c
SM1701 <717513736@qq.com>
2024-06-18 14:22:16 +0800

    flappy dragon: bracket terminal

---
+++ src/flappy_dragon/main.rs
@@ -0,0 +1,18 @@
+use bracket_lib::prelude::*;
+
+fn main() -> BResult<()> {
+    let ctx = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;
+
+    main_loop(ctx, State {})
+}
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, Bracket Terminal!")
+    }
+}
