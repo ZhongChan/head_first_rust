f66065a0260b338d289e6cfcb29e162b846c876a
SM1701 <717513736@qq.com>
2024-06-18 11:23:43 +0800

    treehouse: struct

--- src/treehouse/main.rs
+++ src/treehouse/main.rs
@@ -5,19 +5,44 @@ fn main() {
     let your_name = what_is_your_name();
     println!("{:?}", your_name);
 
-    let visitor_list = ["bert", "steve", "fred"];
+    let visitor_list = [
+        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
+        Visitor::new("steve", "Hello Steve, your milk is in the fridge."),
+        Visitor::new("fred", "Wow, who invited Fred?"),
+    ];
+
     let mut allow_the_in = false;
+    let mut your = Visitor::new("", "");
     for visitor in visitor_list {
-        if your_name == visitor {
+        if your_name == visitor.name {
             allow_the_in = true;
+            your = visitor;
             break;
         }
     }
 
     if allow_the_in {
-        println!("Welcome!");
+        your.greet_visitor();
     } else {
-        println!("Sorry,yor are not in the list")
+        println!("Sorry,yor are not in the list");
+    }
+}
+
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    pub fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting)
     }
 }
 
