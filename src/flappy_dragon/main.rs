f19f3c55cb34aa0264aa9487bcd468c4ad439bd9
SM1701 <717513736@qq.com>
2024-06-18 18:00:27 +0800

    flappy dragon: player gravity and move

--- src/flappy_dragon/main.rs
+++ src/flappy_dragon/main.rs
@@ -102,4 +102,22 @@ impl Player {
     fn render(&mut self, ctx: &mut BTerm) {
         ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'));
     }
+
+    fn gravity_and_move(&mut self) {
+        // 下落速度小于2添加重力
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+
+        self.y += self.velocity as i32; //向下取整
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0
+        }
+    }
+
+    // 煽动翅膀
+    fn flap(&mut self) {
+        self.velocity = -2.0
+    }
 }
