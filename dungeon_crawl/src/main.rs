92888a22dad3a533d8a3eedc91780ecbad7d1e29
SM1701 <717513736@qq.com>
2024-06-19 15:10:02 +0800

    dungeon crawl: player and map with camera

--- dungeon_crawl/src/main.rs
+++ dungeon_crawl/src/main.rs
@@ -63,9 +63,14 @@ impl State {
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(0);
         ctx.cls();
-        self.player.update(ctx, &self.map);
+        
+        ctx.set_active_console(1);
+        ctx.cls();
+        
+        self.player.update(ctx, &self.map, &mut self.camera);
         self.map.render(ctx, &self.camera);
-        self.player.render(ctx);
+        self.player.render(ctx, &self.camera);
     }
 }
