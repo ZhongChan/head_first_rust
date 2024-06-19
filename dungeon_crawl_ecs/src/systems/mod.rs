9eb19c861b91a212199c233fde6c98c3aca63f80
SM1701 <717513736@qq.com>
2024-06-19 18:30:07 +0800

    dungeon crawl esc: map render

--- dungeon_crawl_ecs/src/systems/mod.rs
+++ dungeon_crawl_ecs/src/systems/mod.rs
@@ -1,9 +1,11 @@
 mod player_input;
+mod map_render;
 
 use crate::prelude::*;
 
 pub fn build_schedule() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .add_system(map_render::map_render_system())
         .build()
 }
