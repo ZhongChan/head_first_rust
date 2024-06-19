77d341914e84377164ca5e29c1f9288a7c1a3819
SM1701 <717513736@qq.com>
2024-06-19 09:20:54 +0800

    dungeon crawl: prelude comment

--- dungeon_crawl/src/main.rs
+++ dungeon_crawl/src/main.rs
@@ -1,6 +1,10 @@
 mod map;
 mod player;
 
+/// # 使用其他模块
+/// 模块之间是通过树形结构来组织的，使用 `use` 关键字导入时：
+/// * `super::` 树形结构中使用位于自己相邻上级的模块。
+/// * `crate::` 访问位于树根的模块，也就是 `main.rs`。
 mod prelude {
     pub use bracket_lib::prelude::*;
 
