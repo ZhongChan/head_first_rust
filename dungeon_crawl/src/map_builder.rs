6e277f68ef9d275f545ad5a1df9be7b4fb0260e6
SM1701 <717513736@qq.com>
2024-06-19 11:44:39 +0800

    dungeon crawl: random rooms

--- dungeon_crawl/src/map_builder.rs
+++ dungeon_crawl/src/map_builder.rs
@@ -1,3 +1,4 @@
+use crate::map::TileType::Floor;
 use crate::prelude::*;
 
 /// 房间数量
@@ -18,6 +19,38 @@ impl MapBuilder {
     ///     * `解引用`表示开发者想修改被引用的变量，而不是修改引用本身
     pub fn fill(&mut self, tile_type: TileType) {
         self.map.tiles.iter_mut().for_each(|t| *t = tile_type);
+    }
+
+    pub fn build_random_roms(&mut self, rng: &mut RandomNumberGenerator) {
+
+        // 生成 NUM_ROOMS 个非相交房间
+        while self.roms.len() < NUM_ROOMS {
+            let room = Rect::with_size(
+                rng.range(1, SCREEN_WIDTH - 10),
+                rng.range(1, SCREEN_HEIGHT - 10),
+                rng.range(2, 10),
+                rng.range(2, 10),
+            );
+
+            //是否有房间重叠在一起
+            let mut overloop = false;
+            for r in self.roms.iter() {
+                if r.intersect(&room) {
+                    overloop = true;
+                }
+            }
+
+            // 如果房间不重叠，检查其中每一个点是否都在地图范围内。如果是，就把对应位置改成壁板。
+            if !overloop {
+                room.for_each(|p| {
+                    if p.x > 0 && p.x < SCREEN_WIDTH && p.y > 0 && p.y < SCREEN_HEIGHT {
+                        let idx = map_idx(p.x, p.y);
+                        self.map.tiles[idx] = Floor
+                    }
+                });
+                self.roms.push(room)
+            }
+        }
     }
 }
 
