82259007b35b4b4ff8f9f1e83ee1d0c752bd281f
SM1701 <717513736@qq.com>
2024-06-19 11:16:53 +0800

    dungeon crawl: update player position

--- dungeon_crawl/src/player.rs
+++ dungeon_crawl/src/player.rs
@@ -20,4 +20,27 @@ impl Player {
             to_cp437('@'),
         )
     }
+
+    /// # 更新玩家坐标
+    /// 通键盘的上下左右获取坐标增量
+    /// 当前位置+增量 = 新的坐标
+    /// 如果新坐标可以进入图块，则更新玩家坐标为新坐标
+    pub fn update(&mut self, ctx: &mut BTerm, map: &Map) {
+        if let Some(key) = ctx.key {
+            let delta = match key {
+                VirtualKeyCode::Left => Point::new(-1, 0),
+                VirtualKeyCode::Right => Point::new(1, 0),
+                VirtualKeyCode::Up => Point::new(0, -1),
+                VirtualKeyCode::Down => Point::new(0, 1),
+                _ => Point::zero(),
+            };
+
+            // 当前坐标 + 增量
+
+            let new_position = self.position + delta;
+            if map.can_enter_tile(new_position) {
+                self.position = new_position
+            }
+        }
+    }
 }
