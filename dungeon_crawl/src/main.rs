577c129c070d475f066a80f5e03064d775c681d2
SM1701 <717513736@qq.com>
2024-06-19 09:16:47 +0800

    dungeon crawl: prelude

--- dungeon_crawl/src/main.rs
+++ dungeon_crawl/src/main.rs
@@ -1,6 +1,17 @@
 mod map;
 mod player;
 
+mod prelude {
+    pub use bracket_lib::prelude::*;
+
+    const SCREEN_WIDTH: i32 = 80;
+    const SCREEN_HEIGHT: i32 = 50;
+
+    pub use crate::map::*;
+}
+
+use prelude::*;
+
 fn main() {
     println!("Hello, world!");
 }
