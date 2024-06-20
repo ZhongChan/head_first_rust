3c9d6f671be0a103aa94a0a780ca3d502f2819c3
SM1701 <717513736@qq.com>
2024-06-20 10:10:11 +0800

    dungeon crawl esc: add collision detects system

--- dungeon_crawl_ecs/src/systems/mod.rs
+++ dungeon_crawl_ecs/src/systems/mod.rs
@@ -3,6 +3,8 @@ mod entity_render;
 mod map_render;
 mod player_input;
 
+use collisions::collisions_system;
+
 use crate::prelude::*;
 
 pub fn build_schedule() -> Schedule {
@@ -10,5 +12,6 @@ pub fn build_schedule() -> Schedule {
         .add_system(player_input::player_input_system())
         .add_system(map_render::map_render_system())
         .add_system(entity_render::player_render_system())
+        .add_system(collisions_system())
         .build()
 }
