0f4b75636d1e5b6209132ba602789d63c2b724bd
SM1701 <717513736@qq.com>
2024-06-20 21:37:30 +0800

    dungeon crawl esc: input player monster schedule

--- dungeon_crawl_ecs/src/systems/entity_render.rs
+++ dungeon_crawl_ecs/src/systems/entity_render.rs
@@ -3,7 +3,7 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(Render)]
-pub fn player_render(ecs: &SubWorld, #[resource] camera: &Camera) {
+pub fn entity_render(ecs: &SubWorld, #[resource] camera: &Camera) {
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(1);
     let offset = Point::new(camera.left_x, camera.top_y);
--- dungeon_crawl_ecs/src/systems/mod.rs
+++ dungeon_crawl_ecs/src/systems/mod.rs
@@ -6,6 +6,10 @@ mod player_input;
 mod random_move;
 
 use collisions::collisions_system;
+use end_turn::end_turn_system;
+use entity_render::entity_render_system;
+use map_render::map_render_system;
+use player_input::player_input_system;
 use random_move::random_move_system;
 
 use crate::prelude::*;
@@ -14,9 +18,38 @@ pub fn build_schedule() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
         .add_system(map_render::map_render_system())
-        .add_system(entity_render::player_render_system())
+        .add_system(entity_render::entity_render_system())
         .flush()
         .add_system(collisions_system())
         .add_system(random_move_system())
+        .build()
+}
+
+pub fn build_input_schedule() -> Schedule {
+    Schedule::builder()
+        .add_system(player_input_system())
+        .flush()
+        .add_system(map_render_system())
+        .add_system(entity_render_system())
+        .build()
+}
+
+pub fn build_player_schedule() -> Schedule {
+    Schedule::builder()
+        .add_system(collisions_system())
+        .flush()
+        .add_system(map_render_system())
+        .add_system(entity_render_system())
+        .add_system(end_turn_system())
+        .build()
+}
+
+pub fn build_monster_schedule() -> Schedule {
+    Schedule::builder()
+        .add_system(random_move_system())
+        .flush()
+        .add_system(map_render_system())
+        .add_system(entity_render_system())
+        .add_system(end_turn_system())
         .build()
 }
