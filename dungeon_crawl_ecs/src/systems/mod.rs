ad53f59843a2ee6956817f352e6b462069dda7b6
SM1701 <717513736@qq.com>
2024-06-20 09:26:19 +0800

    dungeon crawl esc: entity player render and add to system

--- dungeon_crawl_ecs/src/systems/mod.rs
+++ dungeon_crawl_ecs/src/systems/mod.rs
@@ -1,5 +1,6 @@
 mod player_input;
 mod map_render;
+mod entity_render;
 
 use crate::prelude::*;
 
@@ -7,5 +8,6 @@ pub fn build_schedule() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
         .add_system(map_render::map_render_system())
+        .add_system(entity_render::player_render_system())
         .build()
 }
