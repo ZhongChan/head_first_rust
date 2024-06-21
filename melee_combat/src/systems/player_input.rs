dbbdc0a0e384c8d06fc03e6ac7bee4e6802d902d
SM1701 <717513736@qq.com>
2024-06-21 16:36:47 +0800

    melee combat: player input move or attact

--- melee_combat/src/systems/player_input.rs
+++ melee_combat/src/systems/player_input.rs
@@ -18,21 +18,44 @@ pub fn player_input(
             _ => Point::new(0, 0),
         };
 
+        // query player
+        let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+
+        let (player_entity, destination) = players
+            .iter(ecs)
+            .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
+            .unwrap();
+
+        // query enimies
+        let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+
         if delta.x != 0 || delta.y != 0 {
-            let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+            let mut hit_something = false;
+            enemies
+                .iter(ecs)
+                .filter(|(_, pos)| **pos == destination)
+                .for_each(|(entity, _)| {
+                    hit_something = true;
+                    commands.push((
+                        (),
+                        WantsToAttact {
+                            attacter: player_entity,
+                            victim: *entity,
+                        },
+                    ));
+                });
 
-            players.iter(ecs).for_each(|(entity, pos)| {
-                let destination = *pos + delta;
-
+            if !hit_something {
                 commands.push((
                     (),
                     WantsToMove {
-                        entity: *entity,
-                        destination,
+                        entity: player_entity,
+                        destination: destination,
                     },
                 ));
-            });
+            }
         }
+
         *turn_state = TrunState::PlayerTurn;
     }
 }
