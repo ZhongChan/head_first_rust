d2728554ef1c3d62552aec3ae74545f8dbafaff8
SM1701 <717513736@qq.com>
2024-06-21 11:27:28 +0800

    dungeon crawl esc: add tooltips system to schduele

--- dungeon_crawl_ecs/src/systems/mod.rs
+++ dungeon_crawl_ecs/src/systems/mod.rs
@@ -11,10 +11,12 @@ mod tooltips;
 use collisions::collisions_system;
 use end_turn::end_turn_system;
 use entity_render::entity_render_system;
+use hud::hud_system;
 use map_render::map_render_system;
 use movement::movement_system;
 use player_input::player_input_system;
 use random_move::random_move_system;
+use tooltips::tooltips_system;
 
 use crate::prelude::*;
 
@@ -24,6 +26,8 @@ pub fn build_input_schedule() -> Schedule {
         .flush()
         .add_system(map_render_system())
         .add_system(entity_render_system())
+        .add_system(hud_system())
+        .add_system(tooltips_system())
         .build()
 }
 
@@ -35,6 +39,7 @@ pub fn build_player_schedule() -> Schedule {
         .flush()
         .add_system(map_render_system())
         .add_system(entity_render_system())
+        .add_system(hud_system())
         .add_system(end_turn_system())
         .build()
 }
@@ -49,6 +54,7 @@ pub fn build_monster_schedule() -> Schedule {
         .flush()
         .add_system(map_render_system())
         .add_system(entity_render_system())
+        .add_system(hud_system())
         .add_system(end_turn_system())
         .build()
 }
