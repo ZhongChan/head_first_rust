a209e55a040c25f04b484100e8e382d2b41440c8
SM1701 <717513736@qq.com>
2024-06-21 09:46:42 +0800

    dungeon crawl esc: message on random move

--- dungeon_crawl_ecs/src/systems/random_move.rs
+++ dungeon_crawl_ecs/src/systems/random_move.rs
@@ -3,9 +3,9 @@ use crate::prelude::*;
 #[system]
 #[write_component(Point)]
 #[read_component(MoveRandomly)]
-pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map) {
-    let mut movers = <(&mut Point, &MoveRandomly)>::query();
-    movers.iter_mut(ecs).for_each(|(pos, _)| {
+pub fn random_move(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
+    let mut movers = <(Entity, &mut Point, &MoveRandomly)>::query();
+    movers.iter_mut(ecs).for_each(|(entity, pos, _)| {
         let mut rng = RandomNumberGenerator::new();
         let destination = match rng.range(0, 4) {
             0 => Point::new(-1, 0),
@@ -14,8 +14,12 @@ pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map) {
             _ => Point::new(0, 1),
         } + *pos;
 
-        if map.can_enter_tile(destination) {
-            *pos = destination;
-        }
-    })
+        commands.push((
+            (),
+            WantsToMove {
+                entity: *entity,
+                destination,
+            },
+        ));
+    });
 }
