cd1c7606e6d24292ccb76da6d1b3878ad462f0c7
SM1701 <717513736@qq.com>
2024-06-21 10:12:54 +0800

    dungeon crawl esc: fix the turn state error

--- dungeon_crawl_ecs/src/systems/end_turn.rs
+++ dungeon_crawl_ecs/src/systems/end_turn.rs
@@ -5,7 +5,7 @@ pub fn end_turn(#[resource] turn_state: &mut TrunState) {
     let new_state = match turn_state {
         TrunState::AwaitingInput => return,
         TrunState::PlayerTurn => TrunState::MonsterTurn,
-        TrunState::MonsterTurn => TrunState::PlayerTurn,
+        TrunState::MonsterTurn => TrunState::AwaitingInput,
     };
 
     *turn_state = new_state;
