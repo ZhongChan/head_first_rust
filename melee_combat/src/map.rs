c1db6a475c1ed6632308864aeefbba2543cff7f8
SM1701 <717513736@qq.com>
2024-06-24 22:02:56 +0800

    melee combat: impl get_available_exits for map

--- melee_combat/src/map.rs
+++ melee_combat/src/map.rs
@@ -85,4 +85,26 @@ impl Algorithm2D for Map {
     }
 }
 
-impl BaseMap for Map {}
+impl BaseMap for Map {
+    fn get_available_exits(&self, idx: usize) -> SmallVec<[(usize, f32); 10]> {
+        let mut exits = SmallVec::new();
+        let location = self.index_to_point2d(idx);
+        if let Some(idx) = self.valid_exit(location, Point::new(-1, 0)) {
+            exits.push((idx, 1.0));
+        };
+        if let Some(idx) = self.valid_exit(location, Point::new(1, 0)) {
+            exits.push((idx, 1.0));
+        };
+        if let Some(idx) = self.valid_exit(location, Point::new(0, -1)) {
+            exits.push((idx, 1.0));
+        };
+        if let Some(idx) = self.valid_exit(location, Point::new(0, 1)) {
+            exits.push((idx, 1.0));
+        };
+        exits
+    }
+
+    fn get_pathing_distance(&self, _idx1: usize, _idx2: usize) -> f32 {
+        1.0
+    }
+}
