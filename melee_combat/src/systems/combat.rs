a6aeace6a3226cce0d0b702507c7a51bf9ed17fa
SM1701 <717513736@qq.com>
2024-06-26 09:47:02 +0800

    melee combat: game crash fix 1. when player helath is zero, hud system get player status,the crash 2. combat system:if is player ,don't remove player

--- melee_combat/src/systems/combat.rs
+++ melee_combat/src/systems/combat.rs
@@ -2,6 +2,7 @@ use crate::prelude::*;
 
 #[system]
 #[read_component(WantsToAttact)]
+#[read_component(Player)]
 #[write_component(Health)]
 pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
     let mut attacters = <(Entity, &WantsToAttact)>::query();
@@ -11,6 +12,13 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
         .collect();
 
     victims.iter().for_each(|(message, victim)| {
+        //player's health zero
+        let is_player = ecs
+            .entry_ref(*victim)
+            .unwrap()
+            .get_component::<Player>()
+            .is_ok();
+
         if let Ok(health) = ecs
             .entry_mut(*victim)
             .unwrap()
@@ -18,7 +26,7 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
         {
             println!("Health before attact: {}", health.current);
             health.current -= 1;
-            if health.current < 1 {
+            if health.current < 1 && !is_player {
                 commands.remove(*victim);
             }
             println!("Health after attact: {}", health.current);
