7cb65f6f41bc9d55ef24a77995eb16fc2cc80ede
SM1701 <717513736@qq.com>
2024-06-30 11:25:59 +0800

    what can i see: 1. update tooltips system 2. hidding monsters hud when player fov not visible

--- what_can_i_see/src/systems/tooltips.rs
+++ what_can_i_see/src/systems/tooltips.rs
@@ -4,11 +4,16 @@ use crate::prelude::*;
 #[read_component(Point)]
 #[read_component(Name)]
 #[read_component(Health)]
+#[read_component(Player)]
+#[read_component(FieldOfView)]
 pub fn tooltips(
     ecs: &SubWorld,
     #[resource] mouse_pos: &Point, // (1)
     #[resource] camera: &Camera,   // (2)
 ) {
+    let mut fov = <&FieldOfView>::query().filter(component::<Player>());
+    let player_fov = fov.iter(ecs).nth(0).unwrap();
+
     let mut positions = <(Entity, &Point, &Name)>::query();
     let offset = Point::new(camera.left_x, camera.top_y);
     let map_pos = *mouse_pos + offset;
@@ -18,7 +23,7 @@ pub fn tooltips(
 
     positions
         .iter(ecs)
-        .filter(|(_, pos, _)| **pos == map_pos) // (5)
+        .filter(|(_, pos, _)| **pos == map_pos && player_fov.visible_tiles.contains(&pos)) // (5)
         .for_each(|(entity, _, name)| {
             let screen_pos = *mouse_pos * 4; // (6)
             let display = if let Ok(health) = ecs
