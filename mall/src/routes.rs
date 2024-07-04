d14487f9262fc3b6d790b613a4275aa878ae9a68
SM1701 <717513736@qq.com>
2024-07-04 10:22:01 +0800

    rust-web:

--- mall/src/routes.rs
+++
@@ -1,7 +0,0 @@
-use crate::handlers::{get_data_from_mysql_1, get_data_from_mysql_2};
-use actix_web::web;
-
-pub fn init(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::resource("/mysql1").route(web::get().to(get_data_from_mysql_1)));
-    cfg.service(web::resource("/mysql2").route(web::get().to(get_data_from_mysql_2)));
-}
