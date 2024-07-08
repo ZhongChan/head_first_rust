178bc6e892c3847ac536675ebdb38071b7705d02
SM1701 <717513736@qq.com>
2024-07-08 11:24:44 +0800

    rust-web ch_03: add new route filter hello

--- rust-web/ch_03/src/main.rs
+++ rust-web/ch_03/src/main.rs
@@ -1,5 +1,10 @@
+use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    println!("Hello");
+    // create a path Filter
+    let path_hello = warp::path("hello").map(|| format!("Hello,Warp Filter!"));
+
+    // start the server and pass the route filter to it
+    warp::serve(path_hello).run(([127, 0, 0, 1], 3030)).await;
 }
