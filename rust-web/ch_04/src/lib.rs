ea05bd7f31bc63ae4bc4321d18411a647601723e
SM1701 <717513736@qq.com>
2024-07-09 13:49:38 +0800

    rust-web ch_04: read json from file

--- rust-web/ch_04/src/lib.rs
+++ rust-web/ch_04/src/lib.rs
@@ -1,6 +1,5 @@
-use std::collections::HashMap;
-
 use ch_02::{Question, QuestionId};
+use std::collections::HashMap;
 
 pub struct Store {
     pub questions: HashMap<QuestionId, Question>,
@@ -9,12 +8,17 @@ pub struct Store {
 impl Store {
     pub fn new() -> Self {
         Store {
-            questions: HashMap::new(),
+            questions: Self::init(),
         }
     }
 }
 
 impl Store {
+    pub fn init() -> HashMap<QuestionId, Question> {
+        let file = include_str!("../question.json");
+        serde_json::from_str(file).expect("can't read question.json")
+    }
+
     pub fn add_question(mut self, question: Question) -> Self {
         self.questions.insert(question.id.clone(), question);
         self
