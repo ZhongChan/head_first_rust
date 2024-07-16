1021d2520b1c3aee9f117674bedc4e6a9321c87b
SM1701 <717513736@qq.com>
2024-07-16 10:57:55 +0800

    rust-web ch_07:

---
+++ rust-web/ch_07/src/types/question.rs
@@ -0,0 +1,29 @@
+use serde::{Deserialize, Serialize};
+use std::fmt::*;
+
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
+pub struct Question {
+    pub id: QuestionId,
+    pub title: String,
+    pub content: String,
+    pub tags: Option<Vec<String>>,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq, Hash)]
+pub struct QuestionId(pub String);
+
+impl Display for Question {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(
+            f,
+            "{}, title: {}, content: {}, tags: {:?}",
+            self.id, self.title, self.content, self.tags
+        )
+    }
+}
+
+impl Display for QuestionId {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "id: {}", self.0)
+    }
+}
