9fb021ecb034b2b7f827c7af22938f069ed3fd97
SM1701 <717513736@qq.com>
2024-07-20 09:38:52 +0800

    rust-web ch_09: 1. use rand,argon2 2. password hashed with random salt

--- rust-web/ch_09/src/routes/authentications.rs
+++ rust-web/ch_09/src/routes/authentications.rs
@@ -1,3 +1,5 @@
+use argon2::Config;
+use rand::Rng;
 use reqwest::StatusCode;
 
 use crate::{store::Store, types::account::NewAccount};
@@ -6,8 +8,21 @@ pub async fn register(
     store: Store,
     account: NewAccount,
 ) -> Result<impl warp::Reply, warp::Rejection> {
+    let hashed_password = hash_password(account.password.as_bytes());
+    let account = NewAccount {
+        nickname: account.nickname,
+        email: account.email,
+        password: hashed_password,
+    };
+
     match store.add_account(account).await {
         Ok(_) => Ok(warp::reply::with_status("Account added", StatusCode::OK)),
         Err(err) => Err(warp::reject::custom(err)),
     }
+}
+
+fn hash_password(password: &[u8]) -> String {
+    let salt = rand::thread_rng().gen::<[u8; 32]>();
+    let config = Config::default();
+    argon2::hash_encoded(password, &salt, &config).unwrap()
 }
