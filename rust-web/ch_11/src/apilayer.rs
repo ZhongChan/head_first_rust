ebc61f023c7d6b9bca37e973a786879178987622
SM1701 <717513736@qq.com>
2024-07-21 18:08:30 +0800

    rust-web ch_11: 1. test api

--- rust-web/ch_11/src/apilayer.rs
+++ rust-web/ch_11/src/apilayer.rs
@@ -3,6 +3,7 @@ use reqwest_retry::{policies::ExponentialBackoff, RetryTransientMiddleware};
 use serde::Deserialize;
 use serde::Serialize;
 use std::env;
+use tracing::Level;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct APIResponse {
@@ -35,7 +36,7 @@ pub struct BadWordsList {
 
 pub async fn check_profanity(content: String) -> Result<String, handle_errors::Error> {
     // layerapi bad words
-    let api_key: String = env::var("APILAYER_KEY").expect("API_KEY not set");
+    let api_key: String = env::var("APILAYER_KEY").expect("APILAYER KEY not set");
 
     //middleware and retry
     let retry_policy = ExponentialBackoff::builder().build_with_max_retries(3);
@@ -70,8 +71,62 @@ pub async fn check_profanity(content: String) -> Result<String, handle_errors::E
 }
 
 async fn transform_error(resp: reqwest::Response) -> handle_errors::APILayerError {
-    handle_errors::APILayerError {
-        status: resp.status().as_u16(),
-        message: resp.json::<APIResponse>().await.unwrap().message,
+    // 提取响应状态码
+    let status = resp.status().as_u16();
+
+    // 提取响应体并转换为字符串
+    let body = resp
+        .text()
+        .await
+        .unwrap_or_else(|_| "Failed to read response body".to_string());
+
+    // 使用 tracing 记录响应体
+    tracing::event!(Level::INFO, "Response body: {}", body);
+
+    // 假设 APIResponse 结构体包含一个 message 字段
+    let message = if let Ok(api_response) = serde_json::from_str::<APIResponse>(&body) {
+        api_response.message
+    } else {
+        "Failed to parse JSON response".to_string()
+    };
+
+    handle_errors::APILayerError { status, message }
+}
+
+#[cfg(test)]
+mod apilayer_tests {
+    use super::{check_profanity, env};
+    use mock_server::{MockServer, OneshotHandler};
+
+    #[tokio::test]
+    async fn run() {
+        let handler = run_mock();
+        censor_profane_words().await;
+        no_profane_words().await;
+        let _ = handler.sender.send(1); //close server
+    }
+
+    fn run_mock() -> OneshotHandler {
+        env::set_var("APILAYER_URL", "http://127.0.0.1:3030");
+        env::set_var("APILAYER_KEY", "YES");
+
+        let socket = "127.0.0.1:3030"
+            .to_string()
+            .parse()
+            .expect("Not a valid address");
+        let mock = MockServer::new(socket);
+        mock.oneshot()
+    }
+
+    async fn censor_profane_words() {
+        let content = "This is a shitty sentence".to_string();
+        let censored_content = check_profanity(content).await;
+        assert_eq!(censored_content.unwrap(), "sdfsd")
+    }
+
+    async fn no_profane_words() {
+        let content = "This is a shitty sentence".to_string();
+        let censored_content = check_profanity(content).await;
+        assert_eq!(censored_content.unwrap(), "sdfsd")
     }
 }
