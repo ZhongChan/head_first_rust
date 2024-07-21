8cbc6749479f1450f6e625e1c26d4f8b53921f06
SM1701 <717513736@qq.com>
2024-07-21 17:44:37 +0800

    rust-web ch_11: 1. mock server

--- rust-web/ch_11/mock_server/src/lib.rs
+++ rust-web/ch_11/mock_server/src/lib.rs
@@ -1,14 +1,76 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{collections::HashMap, net::SocketAddr};
+
+use bytes::Bytes;
+use serde_json::json;
+use tokio::sync::oneshot::{self, Sender};
+use warp::{http, reply::Reply, Filter};
+
+#[derive(Debug, Clone)]
+pub struct MockServer {
+    socket: SocketAddr,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct OneshotHandler {
+    pub sender: Sender<i32>,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl MockServer {
+    pub fn new(bind_addr: SocketAddr) -> Self {
+        MockServer { socket: bind_addr }
+    }
+
+    async fn check_profanity(_: (), content: Bytes) -> Result<impl warp::Reply, warp::Rejection> {
+        let content = String::from_utf8(content.to_vec()).expect("Invalid UTF-8");
+        if content.contains("shitty") {
+            Ok(warp::reply::with_status(
+                warp::reply::json(&json!({
+                "bad_words_list": [
+                {
+                "deviations": 0,
+                "end": 16,
+                "info": 2,
+                "original": "shitty",
+                "replacedLen": 6,
+                "start": 10,
+                "word": "shitty"
+                }
+                ],
+                "bad_words_total": 1,
+                "censored_content": "this is a ****** sentence",
+                "content": "this is a shitty sentence"
+                })),
+                http::StatusCode::OK,
+            ))
+        } else {
+            Ok(warp::reply::with_status(
+                warp::reply::json(&json!({
+                    "bad_words_list":[],
+                    "bad_words_total":0,
+                    "censored_content":"",
+                    "content":"this is a sentence"
+                })),
+                http::StatusCode::OK,
+            ))
+        }
+    }
+
+    fn build_routes(&self) -> impl Filter<Extract = impl Reply> + Clone {
+        warp::post()
+            .and(warp::path("bad_workds"))
+            .and(warp::query())
+            .map(|_: HashMap<String, String>| ())
+            .and(warp::path::end())
+            .and(warp::body::bytes())
+            .and_then(Self::check_profanity)
+    }
+
+    pub fn oneshot(&self) -> OneshotHandler {
+        let (tx, rx) = oneshot::channel::<i32>();
+        let routes = Self::build_routes(&self);
+        let (_, server) = warp::serve(routes).bind_with_graceful_shutdown(self.socket, async {
+            rx.await.ok();
+        });
+        tokio::task::spawn(server);
+        OneshotHandler { sender: tx }
     }
 }
