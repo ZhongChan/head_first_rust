adf5ce12a4cab93bf3faef53db1677c55ffdca27
SM1701 <717513736@qq.com>
2024-09-23 11:26:46 +0800

    zero2prod: 3.5.1.2  Choosing A Random Port

--- zero2prod/src/lib.rs
+++ zero2prod/src/lib.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
@@ -9,13 +11,13 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
     })
-    .bind("127.0.0.1:8000")?
+    .listen(listener)?
     .run();
     Ok(server)
 }
--- zero2prod/src/main.rs
+++ zero2prod/src/main.rs
@@ -1,6 +1,9 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Faild to bind port");
+    run(listener)?.await
 }
