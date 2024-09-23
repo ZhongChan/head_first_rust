6086c33358b1888a3e1b179c308167cb9487fd17
SM1701 <717513736@qq.com>
2024-09-23 11:16:16 +0800

    zero2prod: 3.5.1 tokio::spawn

--- zero2prod/src/lib.rs
+++ zero2prod/src/lib.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -9,13 +9,13 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    .run();
+    Ok(server)
 }
--- zero2prod/tests/health_check.rs
+++ zero2prod/tests/health_check.rs
@@ -1,6 +1,6 @@
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app().await.expect("Faild to spawn our app.");
+    spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
@@ -12,6 +12,7 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() -> Result<(), std::io::Error> {
-    zero2prod::run().await
+fn spawn_app() {
+    let server = zero2prod::run().expect("Faild to bind address.");
+    let _ = tokio::spawn(server);
 }
