63e8c2bbfac10bc25338732d1012d9f269b93304
SM1701 <717513736@qq.com>
2024-09-26 14:57:48 +0800

    zero2prod: 4.3 Logging

--- zero2prod/src/main.rs
+++ zero2prod/src/main.rs
@@ -1,10 +1,14 @@
 use std::net::TcpListener;
 
+use env_logger::Env;
 use sqlx::PgPool;
 use zero2prod::{configrations::get_config, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // `init` dos call `set_logger`
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_config().expect("Failed to read configuration.");
 
     let pg_address = configuration.database.connection_string();
--- zero2prod/src/startup.rs
+++ zero2prod/src/startup.rs
@@ -1,5 +1,5 @@
 use crate::routes::{health_check, subscribe};
-use actix_web::{dev::Server, web, App, HttpServer};
+use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -10,6 +10,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     // Capture `connnection` from the surrounding enviroment
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection_pool.clone())
