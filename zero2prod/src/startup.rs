7571c1d0faf6c4522b549003f53e591280468bc0
SM1701 <717513736@qq.com>
2024-10-16 10:50:33 +0800

    zero2prod: 4.5.14 tracing request_id

--- zero2prod/src/startup.rs
+++ zero2prod/src/startup.rs
@@ -1,7 +1,8 @@
 use crate::routes::{health_check, subscribe};
-use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
+use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // Wrap the connection in a smart pointer
@@ -10,7 +11,8 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     // Capture `connnection` from the surrounding enviroment
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            // Instead of `Logger::default`
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection_pool.clone())
