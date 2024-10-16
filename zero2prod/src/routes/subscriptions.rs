3b7676943c62b17a56d4433fd7213a79cd813104
SM1701 <717513736@qq.com>
2024-10-16 09:56:29 +0800

    zero2prod: 4.5.12 tracing::instrument

--- zero2prod/src/routes/subscriptions.rs
+++ zero2prod/src/routes/subscriptions.rs
@@ -13,21 +13,19 @@ pub struct FormData {
 
 // subscribe
 // (2) Retriving a connection from the application state!
+#[tracing::instrument(
+    name = "Adding a new subscriber", 
+    skip(form, db_pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
 pub async fn subscribe(
     form: web::Form<FormData>,
     db_pool: web::Data<PgPool>, //(2)
 ) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-
-    // Spans, like logs, have an associated level
-    // `info_span` creates a span at the info-level
-    let request_span = tracing::info_span!("Adding a new subscriber.",%request_id,subscriber_email = %form.email,subscriber_name = %form.name);
-
-    // Using `enter` in an async function is a recipe for disaster!
-    let _request_span_gurad = request_span.enter();
-    // `_request_span_gurad` is dropped at the end of `subscribe`
-    // That's when we "exit" the span
-
     let query_span = tracing::info_span!("Saving new subscriber details in the database");
 
     let result = sqlx::query!(
@@ -45,19 +43,9 @@ pub async fn subscribe(
     .await;
 
     match result {
-        Ok(_) => {
-            tracing::info!(
-                "request_id {} - New subscriber details have been saved",
-                request_id
-            );
-            HttpResponse::Ok().finish()
-        }
+        Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
-            tracing::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
