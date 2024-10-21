c3ad78a199da28d03ca2a74e06cd409d981e1400
SM1701 <717513736@qq.com>
2024-10-21 16:23:45 +0800

    zero2prod: 6.8 Insightful Assertion Errors: claims

--- zero2prod/tests/health_check.rs
+++ zero2prod/tests/health_check.rs
@@ -175,3 +175,9 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_empty() {
         );
     }
 }
+
+#[test]
+fn dummy_fail() {
+    let result: Result<&str, &str> = Err("The app crashed due to an IO error");
+    claims::assert_ok!(result);
+}
