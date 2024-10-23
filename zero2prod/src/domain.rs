7cd13de02c8537838a8d4b0622a35cddbc67bf9e
SM1701 <717513736@qq.com>
2024-10-23 10:15:39 +0800

    zero2prod: 6.9 Unit Tests

--- zero2prod/src/domain.rs
+++ zero2prod/src/domain.rs
@@ -19,7 +19,7 @@ impl SubscriberName {
         let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
         let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
 
-        if !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters) {
+        if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
             Err(format!("{} is not a valid subscriber name.", s))
         } else {
             Ok(Self(s))
