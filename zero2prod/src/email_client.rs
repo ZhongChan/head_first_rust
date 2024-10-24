315c2a5dc49f7c82c2698dc6b6a37f8191856b5e
SM1701 <717513736@qq.com>
2024-10-24 16:55:28 +0800

    zero2prod: 7.2.6.1 Error Status Code

--- zero2prod/src/email_client.rs
+++ zero2prod/src/email_client.rs
@@ -47,7 +47,8 @@ impl EmailClient {
             )
             .json(&request_body)
             .send()
-            .await?;
+            .await?
+            .error_for_status()?;
         Ok(())
     }
 }
@@ -64,6 +65,7 @@ struct SendEmailRequest<'a> {
 
 #[cfg(test)]
 mod tests {
+    use claims::assert_err;
     use fake::{
         faker::{
             internet::en::SafeEmail,
@@ -73,7 +75,7 @@ mod tests {
     };
     use secrecy::Secret;
     use wiremock::{
-        matchers::{header, header_exists, method, path},
+        matchers::{any, header, header_exists, method, path},
         Mock, MockServer, ResponseTemplate,
     };
 
@@ -109,6 +111,10 @@ mod tests {
         let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
         let email_client = EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake()));
 
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
         Mock::given(header_exists("x-postmark-server-token")) // The matching conditions for a mock are specified using Mock::given;
             .and(header("Content-Type", "application/json"))
             .and(path("/email"))
@@ -119,15 +125,37 @@ mod tests {
             .mount(&mock_server) // wiremock::Mock becomes effective only after it has ben monunted on a wiremock:MockServer
             .await;
 
-        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
-        let subject: String = Sentence(1..2).fake();
-        let content: String = Paragraph(1..10).fake();
-
         // Act
         let _ = email_client
             .send_email(subscriber_email, &subject, &content, &content)
             .await;
 
         // Assert
+    }
+
+    #[tokio::test]
+    async fn send_email_fails_if_the_server_returns_500() {
+        // Arrange
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake()));
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(500)) // a 200 OK response without a body.
+            .expect(1)
+            .mount(&mock_server) // wiremock::Mock becomes effective only after it has ben monunted on a wiremock:MockServer
+            .await;
+
+        // Act
+        let outcome = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+
+        // Assert
+        assert_err!(outcome);
     }
 }
